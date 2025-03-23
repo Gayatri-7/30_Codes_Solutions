8fcc67d2fcd5160b79af12027ec27ff33c4ae5b7
g7 <99476075+Gayatri-7@users.noreply.github.com>
2025-03-23 06:08:56 +0530

    Update Arrayequal.java

--- Build/Arrayequal.java
+++ Build/Arrayequal.java
@@ -20,6 +20,7 @@ public class Arrayequal {
             for (int i = 0; i < arr.length; i++) {
                 if (arr[i] != arr2[i]) {
                     status = false;
+                    break; //imp
 
                 }
             }
